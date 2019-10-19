805e18540dd1467b01c776c395c35958fd04e6e6
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-10-19 22:17:39 +0100

    Error handling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+const debug = require('debug')
+
 const getGmail = require('./lib/gmail')
 
 const {
@@ -5,33 +8,53 @@ const {
   getMessage
 } = require('./lib/gmail/messages')
 
+const error = debug('@sequencemedia/gmail-api:error')
+
 async function app () {
-  const gmail = await getGmail()
+  try {
+    const gmail = await getGmail()
 
-  {
-    const messagesList = await getMessagesList(gmail, { max: 1 })
+    {
+      const messagesList = await getMessagesList(gmail, { max: 1 })
 
-    console.log('length (1)', messagesList.length)
+      console.log('length (1)', messagesList.length)
 
-    Promise.all(messagesList.map((message) => getMessage(gmail, message)))
-      .then((messagesList) => {
-        messagesList.forEach(({ data: { payload } }) => {
-          console.log(payload)
+      Promise.all(messagesList.map((message) => getMessage(gmail, message)))
+        .then((messagesList) => {
+          messagesList.forEach(({ data: { payload } }) => {
+            console.log(payload)
+          })
         })
-      })
-  }
+    }
 
-  {
-    const messagesList = await getMessagesList(gmail, { query: 'foyles', max: 1 })
+    {
+      const messagesList = await getMessagesList(gmail, { query: 'foyles', max: 1 })
 
-    console.log('length (2)', messagesList.length)
+      console.log('length (2)', messagesList.length)
 
-    Promise.all(messagesList.map((message) => getMessage(gmail, message)))
-      .then((messagesList) => {
-        messagesList.forEach(({ data: { payload } }) => {
-          console.log(payload)
+      Promise.all(messagesList.map((message) => getMessage(gmail, message)))
+        .then((messagesList) => {
+          messagesList.forEach(({ data: { payload } }) => {
+            console.log(payload)
+          })
         })
-      })
+    }
+  } catch (e) {
+    const {
+      code
+    } = e
+
+    if (Number(code) === 401) error('Not authorised')
+    else {
+      const {
+        message
+      } = e
+
+      if (code) error(code, message)
+      else {
+        error(message)
+      }
+    }
   }
 }
 
