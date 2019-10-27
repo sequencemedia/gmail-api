02b6a2a7f55677f5a0bc22aad47baa39b009428e
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-10-27 15:52:32 +0000

    Jest

--- test/lib/constants.spec.js
+++ test/lib/constants.spec.js
@@ -2,7 +2,11 @@ const {
   hasCredentialsPath: hasCredentialsPathArgValue,
   getCredentialsPath: getCredentialsPathArgValue,
   hasCredentialsTokenPath: hasCredentialsTokenPathArgValue,
-  getCredentialsTokenPath: getCredentialsTokenPathArgValue
+  getCredentialsTokenPath: getCredentialsTokenPathArgValue,
+  hasCredentialsJson: hasCredentialsJsonArgValue,
+  getCredentialsJson: getCredentialsJsonArgValue,
+  hasCredentialsTokenJson: hasCredentialsTokenJsonArgValue,
+  getCredentialsTokenJson: getCredentialsTokenJsonArgValue
 } = require('@sequencemedia/gmail-api/lib/argv')
 
 const {
@@ -98,7 +102,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
 
   describe('`getCredentialsPath()`', () => {
     describe('A credentials path has been supplied as a command line argument', () => {
-      let credentialsPath
+      let returnValue
 
       beforeEach(() => {
         hasCredentialsPathArgValue.mockReturnValue(true)
@@ -107,7 +111,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
         jest.spyOn(Reflect, 'has').mockReturnValue(false)
         jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
 
-        credentialsPath = getCredentialsPath()
+        returnValue = getCredentialsPath()
       })
 
       afterEach(() => {
@@ -135,14 +139,14 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
       })
 
       it('returns the command line argument value', () => {
-        expect(credentialsPath)
+        expect(returnValue)
           .toBe('MOCK CREDENTIALS PATH COMMAND LINE ARGUMENT')
       })
     })
 
     describe('A credentials path has not been supplied as a command line argument', () => {
       describe('A credentials path has been supplied as an environment variable', () => {
-        let credentialsPath
+        let returnValue
 
         beforeEach(() => {
           hasCredentialsPathArgValue.mockReturnValue(false)
@@ -151,7 +155,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
           jest.spyOn(Reflect, 'has').mockReturnValue(true)
           jest.spyOn(Reflect, 'get').mockReturnValue('MOCK CREDENTIALS PATH ENVIRONMENT VARIABLE')
 
-          credentialsPath = getCredentialsPath()
+          returnValue = getCredentialsPath()
         })
 
         afterEach(() => {
@@ -179,13 +183,13 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
         })
 
         it('returns the environment variable value', () => {
-          expect(credentialsPath)
+          expect(returnValue)
             .toBe('MOCK CREDENTIALS PATH ENVIRONMENT VARIABLE')
         })
       })
 
       describe('A credentials path has not been supplied as an environment variable', () => {
-        let credentialsPath
+        let returnValue
 
         beforeEach(() => {
           hasCredentialsPathArgValue.mockReturnValue(false)
@@ -194,7 +198,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
           jest.spyOn(Reflect, 'has').mockReturnValue(false)
           jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
 
-          credentialsPath = getCredentialsPath()
+          returnValue = getCredentialsPath()
         })
 
         afterEach(() => {
@@ -222,7 +226,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
         })
 
         it('returns the default value', () => {
-          expect(credentialsPath)
+          expect(returnValue)
             .toBe(CREDENTIALS_PATH)
         })
       })
@@ -231,7 +235,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
 
   describe('`getCredentialsTokenPath()`', () => {
     describe('A credentials token path has been supplied as a command line argument', () => {
-      let credentialsTokenPath
+      let returnValue
 
       beforeEach(() => {
         hasCredentialsTokenPathArgValue.mockReturnValue(true)
@@ -240,7 +244,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
         jest.spyOn(Reflect, 'has').mockReturnValue(false)
         jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
 
-        credentialsTokenPath = getCredentialsTokenPath()
+        returnValue = getCredentialsTokenPath()
       })
 
       afterEach(() => {
@@ -268,14 +272,14 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
       })
 
       it('returns the command line argument value', () => {
-        expect(credentialsTokenPath)
+        expect(returnValue)
           .toBe('MOCK CREDENTIALS TOKEN PATH COMMAND LINE ARGUMENT')
       })
     })
 
     describe('A credentials token path has not been supplied as a command line argument', () => {
       describe('A credentials token path has been supplied as an environment variable', () => {
-        let credentialsTokenPath
+        let returnValue
 
         beforeEach(() => {
           hasCredentialsTokenPathArgValue.mockReturnValue(false)
@@ -284,7 +288,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
           jest.spyOn(Reflect, 'has').mockReturnValue(true)
           jest.spyOn(Reflect, 'get').mockReturnValue('MOCK CREDENTIALS TOKEN PATH ENVIRONMENT VARIABLE VALUE')
 
-          credentialsTokenPath = getCredentialsTokenPath()
+          returnValue = getCredentialsTokenPath()
         })
 
         afterEach(() => {
@@ -312,13 +316,13 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
         })
 
         it('returns the environment variable value', () => {
-          expect(credentialsTokenPath)
+          expect(returnValue)
             .toBe('MOCK CREDENTIALS TOKEN PATH ENVIRONMENT VARIABLE VALUE')
         })
       })
 
       describe('A credentials token path has not been supplied an environment variable', () => {
-        let credentialsTokenPath
+        let returnValue
 
         beforeEach(() => {
           hasCredentialsTokenPathArgValue.mockReturnValue(false)
@@ -327,7 +331,7 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
           jest.spyOn(Reflect, 'has').mockReturnValue(false)
           jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
 
-          credentialsTokenPath = getCredentialsTokenPath()
+          returnValue = getCredentialsTokenPath()
         })
 
         afterEach(() => {
@@ -355,8 +359,460 @@ describe('`@sequencemedia/gmail-api/lib/constants`', () => {
         })
 
         it('returns the default value', () => {
-          expect(credentialsTokenPath)
+          expect(returnValue)
             .toBe(CREDENTIALS_TOKEN_PATH)
+        })
+      })
+    })
+  })
+
+  describe('`hasCredentialsJson()`', () => {
+    describe('The credentials json has been supplied as a command line argument', () => {
+      let returnValue
+
+      beforeEach(() => {
+        hasCredentialsJsonArgValue.mockReturnValue(true)
+
+        jest.spyOn(Reflect, 'has')
+
+        returnValue = hasCredentialsJson()
+      })
+
+      afterEach(() => {
+        hasCredentialsJsonArgValue.mockReset()
+
+        Reflect.has.mockRestore()
+      })
+
+      it('interrogates the command line arguments', () => {
+        expect(hasCredentialsJsonArgValue)
+          .toHaveBeenCalled()
+      })
+
+      it('does not interrogate the environment variables', () => {
+        expect(Reflect.has)
+          .not.toHaveBeenCalled()
+      })
+
+      it('returns true', () => {
+        expect(returnValue)
+          .toBe(true)
+      })
+    })
+
+    describe('The credentials json has not been supplied as a command line argument', () => {
+      describe('The credentials json has been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsJsonArgValue.mockReturnValue(false)
+
+          jest.spyOn(Reflect, 'has').mockReturnValue(true)
+
+          returnValue = hasCredentialsJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsJsonArgValue.mockReset()
+
+          Reflect.has.mockRestore()
+        })
+
+        it('interrogates the command line arguments', () => {
+          expect(hasCredentialsJsonArgValue)
+            .toHaveBeenCalled()
+        })
+
+        it('interrogates the environment variables', () => {
+          expect(Reflect.has)
+            .toHaveBeenCalled()
+        })
+
+        it('returns true', () => {
+          expect(returnValue)
+            .toBe(true)
+        })
+      })
+
+      describe('The credentials json has not been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsJsonArgValue.mockReturnValue(false)
+
+          jest.spyOn(Reflect, 'has').mockReturnValue(false)
+
+          returnValue = hasCredentialsJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsJsonArgValue.mockReset()
+
+          Reflect.has.mockRestore()
+        })
+
+        it('interrogates the command line arguments', () => {
+          expect(hasCredentialsJsonArgValue)
+            .toHaveBeenCalled()
+        })
+
+        it('interrogates the environment variables', () => {
+          expect(Reflect.has)
+            .toHaveBeenCalled()
+        })
+
+        it('returns false', () => {
+          expect(returnValue)
+            .toBe(false)
+        })
+      })
+    })
+  })
+
+  describe('`getCredentialsJson()`', () => {
+    describe('The credentials json has been supplied as a command line argument', () => {
+      let returnValue
+
+      beforeEach(() => {
+        hasCredentialsJsonArgValue.mockReturnValue(true)
+        getCredentialsJsonArgValue.mockReturnValue('MOCK CREDENTIALS JSON COMMAND LINE ARGUMENT')
+
+        jest.spyOn(Reflect, 'has').mockReturnValue(false)
+        jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
+
+        returnValue = getCredentialsJson()
+      })
+
+      afterEach(() => {
+        hasCredentialsJsonArgValue.mockReset()
+        getCredentialsJsonArgValue.mockReset()
+
+        Reflect.has.mockRestore()
+        Reflect.get.mockRestore()
+      })
+
+      it('gets the command line argument value', () => {
+        expect(hasCredentialsJsonArgValue)
+          .toHaveBeenCalled()
+
+        expect(getCredentialsJsonArgValue)
+          .toHaveBeenCalled()
+      })
+
+      it('does not get the environment variable value', () => {
+        expect(Reflect.has)
+          .not.toHaveBeenCalled()
+
+        expect(Reflect.get)
+          .not.toHaveBeenCalled()
+      })
+
+      it('returns the command line argument value', () => {
+        expect(returnValue)
+          .toBe('MOCK CREDENTIALS JSON COMMAND LINE ARGUMENT')
+      })
+    })
+
+    describe('The credentials json has not been supplied as a command line argument', () => {
+      describe('The credentials json has been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsJsonArgValue.mockReturnValue(false)
+          getCredentialsJsonArgValue.mockReturnValue(undefined)
+
+          jest.spyOn(Reflect, 'get').mockReturnValue('MOCK CREDENTIALS JSON ENVIRONMENT VARIABLE')
+
+          returnValue = getCredentialsJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsJsonArgValue.mockReset()
+          getCredentialsJsonArgValue.mockReset()
+
+          Reflect.get.mockRestore()
+        })
+
+        it('does not get the command line argument value', () => {
+          expect(hasCredentialsJsonArgValue)
+            .toHaveBeenCalled()
+
+          expect(getCredentialsJsonArgValue)
+            .not.toHaveBeenCalled()
+        })
+
+        it('gets the environment variable value', () => {
+          expect(Reflect.get)
+            .toHaveBeenCalled()
+        })
+
+        it('returns the environment variable value', () => {
+          expect(returnValue)
+            .toBe('MOCK CREDENTIALS JSON ENVIRONMENT VARIABLE')
+        })
+      })
+
+      describe('The credentials json has not been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsJsonArgValue.mockReturnValue(false)
+          getCredentialsJsonArgValue.mockReturnValue(undefined)
+
+          jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
+
+          returnValue = getCredentialsJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsJsonArgValue.mockReset()
+          getCredentialsJsonArgValue.mockReset()
+
+          Reflect.get.mockRestore()
+        })
+
+        it('does not get the command line argument value', () => {
+          expect(hasCredentialsJsonArgValue)
+            .toHaveBeenCalled()
+
+          expect(getCredentialsJsonArgValue)
+            .not.toHaveBeenCalled()
+        })
+
+        it('gets the environment variable value', () => {
+          expect(Reflect.get)
+            .toHaveBeenCalled()
+        })
+
+        it('returns undefined', () => {
+          expect(returnValue)
+            .toBeUndefined()
+        })
+      })
+    })
+  })
+
+  describe('`hasCredentialsTokenJson()`', () => {
+    describe('The credentials token json has been supplied as a command line argument', () => {
+      let returnValue
+
+      beforeEach(() => {
+        hasCredentialsTokenJsonArgValue.mockReturnValue(true)
+
+        jest.spyOn(Reflect, 'has')
+
+        returnValue = hasCredentialsTokenJson()
+      })
+
+      afterEach(() => {
+        hasCredentialsTokenJsonArgValue.mockReset()
+
+        Reflect.has.mockRestore()
+      })
+
+      it('interrogates the command line arguments', () => {
+        expect(hasCredentialsTokenJsonArgValue)
+          .toHaveBeenCalled()
+      })
+
+      it('does not interrogate the environment variables', () => {
+        expect(Reflect.has)
+          .not.toHaveBeenCalled()
+      })
+
+      it('returns true', () => {
+        expect(returnValue)
+          .toBe(true)
+      })
+    })
+
+    describe('The credentials token json has not been supplied as a command line argument', () => {
+      describe('The credentials token json has been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReturnValue(false)
+
+          jest.spyOn(Reflect, 'has').mockReturnValue(true)
+
+          returnValue = hasCredentialsTokenJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReset()
+
+          Reflect.has.mockRestore()
+        })
+
+        it('interrogates the command line arguments', () => {
+          expect(hasCredentialsTokenJsonArgValue)
+            .toHaveBeenCalled()
+        })
+
+        it('interrogates the environment variables', () => {
+          expect(Reflect.has)
+            .toHaveBeenCalled()
+        })
+
+        it('returns true', () => {
+          expect(returnValue)
+            .toBe(true)
+        })
+      })
+
+      describe('The credentials token json has not been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReturnValue(false)
+
+          jest.spyOn(Reflect, 'has').mockReturnValue(false)
+
+          returnValue = hasCredentialsTokenJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReset()
+
+          Reflect.has.mockRestore()
+        })
+
+        it('interrogates the command line arguments', () => {
+          expect(hasCredentialsTokenJsonArgValue)
+            .toHaveBeenCalled()
+        })
+
+        it('interrogates the environment variables', () => {
+          expect(Reflect.has)
+            .toHaveBeenCalled()
+        })
+
+        it('returns false', () => {
+          expect(returnValue)
+            .toBe(false)
+        })
+      })
+    })
+  })
+
+  describe('`getCredentialsTokenJson()`', () => {
+    describe('The credentials token json has been supplied as a command line argument', () => {
+      let returnValue
+
+      beforeEach(() => {
+        hasCredentialsTokenJsonArgValue.mockReturnValue(true)
+        getCredentialsTokenJsonArgValue.mockReturnValue('MOCK CREDENTIALS TOKEN JSON COMMAND LINE ARGUMENT')
+
+        jest.spyOn(Reflect, 'has').mockReturnValue(false)
+        jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
+
+        returnValue = getCredentialsTokenJson()
+      })
+
+      afterEach(() => {
+        hasCredentialsTokenJsonArgValue.mockReset()
+        getCredentialsTokenJsonArgValue.mockReset()
+
+        Reflect.has.mockRestore()
+        Reflect.get.mockRestore()
+      })
+
+      it('gets the command line argument value', () => {
+        expect(hasCredentialsTokenJsonArgValue)
+          .toHaveBeenCalled()
+
+        expect(getCredentialsTokenJsonArgValue)
+          .toHaveBeenCalled()
+      })
+
+      it('does not get the environment variable value', () => {
+        expect(Reflect.has)
+          .not.toHaveBeenCalled()
+
+        expect(Reflect.get)
+          .not.toHaveBeenCalled()
+      })
+
+      it('returns the command line argument value', () => {
+        expect(returnValue)
+          .toBe('MOCK CREDENTIALS TOKEN JSON COMMAND LINE ARGUMENT')
+      })
+    })
+
+    describe('The credentials token json has not been supplied as a command line argument', () => {
+      describe('The credentials token json has been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReturnValue(false)
+          getCredentialsTokenJsonArgValue.mockReturnValue(undefined)
+
+          jest.spyOn(Reflect, 'get').mockReturnValue('MOCK CREDENTIALS TOKEN JSON ENVIRONMENT VARIABLE')
+
+          returnValue = getCredentialsTokenJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReset()
+          getCredentialsTokenJsonArgValue.mockReset()
+
+          Reflect.get.mockRestore()
+        })
+
+        it('does not get the command line argument value', () => {
+          expect(hasCredentialsTokenJsonArgValue)
+            .toHaveBeenCalled()
+
+          expect(getCredentialsTokenJsonArgValue)
+            .not.toHaveBeenCalled()
+        })
+
+        it('gets the environment variable value', () => {
+          expect(Reflect.get)
+            .toHaveBeenCalled()
+        })
+
+        it('returns the environment variable value', () => {
+          expect(returnValue)
+            .toBe('MOCK CREDENTIALS TOKEN JSON ENVIRONMENT VARIABLE')
+        })
+      })
+
+      describe('The credentials token json has not been supplied as an environment variable', () => {
+        let returnValue
+
+        beforeEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReturnValue(false)
+          getCredentialsTokenJsonArgValue.mockReturnValue(undefined)
+
+          jest.spyOn(Reflect, 'get').mockReturnValue(undefined)
+
+          returnValue = getCredentialsTokenJson()
+        })
+
+        afterEach(() => {
+          hasCredentialsTokenJsonArgValue.mockReset()
+          getCredentialsTokenJsonArgValue.mockReset()
+
+          Reflect.get.mockRestore()
+        })
+
+        it('does not get the command line argument value', () => {
+          expect(hasCredentialsTokenJsonArgValue)
+            .toHaveBeenCalled()
+
+          expect(getCredentialsTokenJsonArgValue)
+            .not.toHaveBeenCalled()
+        })
+
+        it('gets the environment variable value', () => {
+          expect(Reflect.get)
+            .toHaveBeenCalled()
+        })
+
+        it('returns undefined', () => {
+          expect(returnValue)
+            .toBeUndefined()
         })
       })
     })
