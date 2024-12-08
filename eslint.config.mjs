ab5cbce3f925dcd3f586bd8396378082ceec2337
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-12-08 12:39:45 +0000

    Types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ export default (
       '**/*.{cjs,mjs}'
     ],
     ignores: [
+      'coverage',
       'test'
     ],
     languageOptions: {
--- index.mjs
+++ index.mjs
@@ -1,1 +1,1 @@
-export * from './src/index.mjs'
+export { default } from './src/index.mjs'
